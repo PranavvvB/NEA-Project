ce95ec2409393885c14185f9cfccf7e5825122c0
PranavvvB <p_bhandare18@cranford.hounslow.sch.uk>
2024-08-20 10:59:13 +0100

    setup the signup page foundations

--- app.py
+++ app.py
@@ -4,14 +4,22 @@ from wtform_fields import *
 app = Flask(__name__)
 app.secret_key = "REPLACE KEY LATER"
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
+# @app.route('/', methods=["GET"])
+# def index():
 
-    registration_form = RegistrationForm()
+#     return render_template("index.html")
 
-    return render_template("index.html", form=registration_form)
+@app.route('/', methods=["GET",'POST'])
+def signin():
 
-if __name__ == '__main__':
+    signup_form = SigninForm()
+    if signup_form.validate_on_submit():
+        return "Sign Up successful"
+    
+
+    return render_template("signup.html", form=signup_form)
+
+   
+if __name__ == "__main__":
     app.run(debug=True)
-
 # app local browser link : http://127.0.0.1:5000/
--- wtform_fields.py
+++ wtform_fields.py
@@ -1,9 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
-from wtforms.validators import InputRequired, Email, Length
+from wtforms import StringField, PasswordField, EmailField, SubmitField
+from wtforms.validators import InputRequired, Email, Length, EqualTo
 
-class RegistrationForm(FlaskForm):
+class SigninForm(FlaskForm):
     
-    username = StringField("username", )
-    password = PasswordField("password")
-    confirm_password = PasswordField("confirm_password")
+    username = StringField("username", validators=[InputRequired(message="Please enter a username"), Length(min=4, max=15, message="Username must be between 4 and 15 characters")])
+    email = EmailField("email", validators=[InputRequired(message="Please enter your email address"), Email(message="Invalid email")])
+    password = PasswordField("password", validators=[InputRequired(message="Please enter a password"), Length(min=4, max=15, message="Password must be between 4 and 15 characters")])
+    confirm_password = PasswordField("confirm_password", validators=[InputRequired(message="Please re-enter your password"), EqualTo('password', message="Passwords must match with the password above")])
+    submit_button = SubmitField()
+
+class LoginForm(FlaskForm):
+    username = StringField("username", validators=[InputRequired(message="Please enter your username")])
+    password = PasswordField("password", validators=[InputRequired(message="Please enter your password")])
